e3e2eeee4430ba78b853bbdb5aec8061d58df8cf
Lion Kortlepel <development@kortlepel.com>
2021-06-27 23:33:06 +0200

    improve unit test result printing

--- src/UnitTest.cpp
+++ src/UnitTest.cpp
@@ -5,7 +5,7 @@ size_t LK::UnitTest::fail_count = 0;
 size_t LK::UnitTest::success_count = 0;
 
 void LK::UnitTest::finalize() {
-    printf("DONE. Failed %lu/%lu (%0.1f%% succeeded).\n", fail_count, fail_count + success_count, (1 - (fail_count ? double(fail_count) / double(fail_count + success_count) : 0.0)) * 100);
+    printf("DONE. %0.1f%% succeeded (failed %lu/%lu).\n", (1 - (fail_count ? double(fail_count) / double(fail_count + success_count) : 0.0)) * 100, fail_count, fail_count + success_count);
     if (fail_count == 0) {
         exit(EXIT_SUCCESS);
     } else {
